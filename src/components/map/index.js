dc5e764f9387046f271af17bf3cdbf524c761342
goodness <goodyc474@gmail.com>
2023-09-04 21:52:27 +0100

    ch: fix issues

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -4,7 +4,7 @@ import { useEffect, useRef, useState } from "react";
 import Button from "./../../components/Button/index";
 import "@tomtom-international/web-sdk-maps/dist/maps.css";
 
-function Map({ pickup }) {
+function Map({ pickup, dropoff }) {
   // const [map, setMap] = useState({});
   // 3.406448;
   // 6.465422;
@@ -13,10 +13,13 @@ function Map({ pickup }) {
   // let center = [-0.112869, 51.504];
   // -7.4081 23.10687
   // 45.42042 - 75.69243;/
-  const [longitude, setLongitude] = useState(3.406448);
-  const [latitude, setLatitude] = useState(6.465422);
+  const [longitude, setLongitude] = useState(-0.112869);
+  const [latitude, setLatitude] = useState(51.504);
   // console.log(...pickup);
-
+  // 9.0765° N, 7.3986°
+  // 7.491302;9.072264; abuja
+  // 23.10687 - 7.4081; kanu
+  // 8.53486 11.99997
   useEffect(() => {
     const map = tt.map({
       key: "42sj3JewKtwZSgwb8lSmGKThXJsp0ZxO",
@@ -30,34 +33,35 @@ function Map({ pickup }) {
     });
     // setMap(map);
     if (pickup) {
-      let [lon, lat] = pickup?.slice();
+      let [lat, lon] = pickup?.slice();
       // let lat = pickup[1];
       setLongitude(lon);
       setLatitude(lat);
-      console.log(lon, lat);
-      // addToMap(map, [lon, lat]);
+      console.log(lat, lon);
+      addToMap(map, [lon, lat]);
     }
-    // if (dropoff) {
-    //   let lon = dropoff[0];
-    //   let lat = dropoff[1];
-    //   setLongitude(lon);
-    //   setLatitude(lat);
-    //   addToMap(map, [lon, lat]);
-    // }
+    if (dropoff) {
+      let [lat, lon] = pickup?.slice();
+
+      setLongitude(lon);
+      setLatitude(lat);
+      addToMap(map, [lon, lat]);
+      console.log(lat, lon);
+    }
 
     // return () => map.remove();
-  }, [pickup]);
+  }, [pickup, dropoff]);
 
-  // const addToMap = (map, cordinate) => {
-  //   new tt.Marker({
-  //     draggable: true,
-  //   })
-  //     .setLngLat(cordinate)
-  //     .addTo(map);
-  //   // map.fitBounds([cordinate], {
-  //   //   padding: 30,
-  //   // });
-  // };
+  const addToMap = (map, cordinate) => {
+    new tt.Marker({
+      draggable: true,
+    })
+      .setLngLat(cordinate)
+      .addTo(map);
+    // map.fitBounds([cordinate], {
+    //   padding: 30,
+    // });
+  };
   return (
     <div className="relative h-[50%] overflow-hidden w-full items-center justify-center">
       <div ref={mapEelement} className="h-[600px] w-full p-5 "></div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -14,6 +14,55 @@ export default function Home() {
   const { currentUser } = useContext(context);
   const [pickUp, setPickUp] = useState("");
   const [dropOff, setDropOff] = useState("");
+  const [newPickUp, setNewPickUp] = useState(null);
+  const [newDrop, setNewDrop] = useState(null);
+
+  const getGeocordinateLoction = async (value) => {
+    try {
+      const res = await fetch(
+        `https://api.tomtom.com/search/2/geocode/${value}.json?key=42sj3JewKtwZSgwb8lSmGKThXJsp0ZxO`
+      );
+      const data = await res.json();
+      if (data) {
+        const result = data?.results[0]?.position;
+        const outPut = Object?.values(result);
+        console.log(data);
+        setNewPickUp(outPut);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const getGeocordinateDest = async (value) => {
+    try {
+      const res = await fetch(
+        `https://api.tomtom.com/search/2/geocode/${value}.json?key=42sj3JewKtwZSgwb8lSmGKThXJsp0ZxO`
+      );
+      const data = await res.json();
+      if (data) {
+        const result = data?.results[0]?.position;
+        const outPut = Object?.values(result);
+        setNewDrop(outPut);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    await getGeocordinateLoction(pickUp);
+    await getGeocordinateLoction(dropOff);
+  };
+  console.log(newPickUp);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setloading(false);
+  //     func();
+  //   }, 2000);
+  //   return () => clearTimeout(timer);
+  // }, [pick]);
 
   return (
     <main className="h-screen relative ">
@@ -25,9 +74,13 @@ export default function Home() {
           className="rounded-full w-10 h-10"
         />
       </nav>
-      <Map />
+      <Map pickup={newPickUp} dropoff={newDrop} />
       <div className="bg-black rounded-t-3xl h-[50%] flex justify-center  items-center ">
-        <div action="" className="flex flex-col p-4 h-full mt-6 w-full">
+        <form
+          action=""
+          className="flex flex-col p-4 h-full mt-6 w-full"
+          onSubmit={handleClick}
+        >
           <p className="m-3 text-white text-center text-2xl">
             Where are you going?
           </p>
@@ -51,19 +104,20 @@ export default function Home() {
             className="bg-purple text-white py-3 rounded-md my-3"
             type="submit"
           >
-            <Link
-              to={
-                pickUp === "" && dropOff === "" ? "" : CLIENT_ROUTHS.chooseride
-              }
-              state={{ pickup: pickUp, dropoff: dropOff }}
-            >
-              Next
-            </Link>
+            next
           </Button>
-        </div>
+        </form>
       </div>
       {/* <div className=" flex-1"></div> */}
     </main>
   );
 }
 // npm i @tomtom-international/web-sdk-maps
+{
+  /* <Link
+  to={pickUp === "" && dropOff === "" ? "" : CLIENT_ROUTHS.home}
+  state={{ pickup: pickUp, dropoff: dropOff }}
+>
+  Next
+</Link>; */
+}
